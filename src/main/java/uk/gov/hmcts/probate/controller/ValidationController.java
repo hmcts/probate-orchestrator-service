3d98a583a39e3afc896f3a8d8185d2b33099769d
ruban72 <ruban72@hotmail.com>
2018-11-29 10:20:34 +0000

    PRI-63: Sonar Lint fixes

--- src/main/java/uk/gov/hmcts/probate/controller/ValidationController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/ValidationController.java
@@ -5,14 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import uk.gov.hmcts.probate.client.validation.BusinessValidationClient;
-import uk.gov.hmcts.probate.controller.mapper.FormDataMapper;
-import uk.gov.hmcts.probate.dto.CaseInfoDTO;
 import uk.gov.hmcts.probate.dto.formdata.FormDataDTO;
 import uk.gov.hmcts.probate.dto.validation.BusinessValidationResponseDTO;
-import uk.gov.hmcts.probate.service.OrchestratorSubmitService;
 import uk.gov.hmcts.probate.service.ValidationService;
 
 @Api(tags = {"ValidationController"})
@@ -33,7 +28,7 @@ public class ValidationController {
             @ApiResponse(code = 200, message = "Validation call to orchestrator successful"),
             @ApiResponse(code = 400, message = "Validation  call to orchestrator failed"),
     })
-    @RequestMapping(path = INTESTACY_VALIDATION_URL, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = INTESTACY_VALIDATION_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<BusinessValidationResponseDTO> validateIntestacy(@RequestBody FormDataDTO formDataDTO) {
         BusinessValidationResponseDTO businessValidationResponseDTO = validationService.validate(formDataDTO, ValidationService.ProbateType.INTESTACY);
