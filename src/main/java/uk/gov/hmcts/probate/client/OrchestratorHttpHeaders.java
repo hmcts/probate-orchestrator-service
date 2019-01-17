b9c65669c790b723b15ee6ab5ee70aaecad6fca9
ruban72 <ruban72@hotmail.com>
2019-01-17 15:10:22 +0000

    PRI-66: increase code coverage

--- src/main/java/uk/gov/hmcts/probate/client/OrchestratorHttpHeaders.java
+++ src/main/java/uk/gov/hmcts/probate/client/OrchestratorHttpHeaders.java
@@ -5,7 +5,4 @@ public class OrchestratorHttpHeaders {
     public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
     public static final String APPLICANT_EMAIL = "applicantEmail";
-
-    private OrchestratorHttpHeaders(){
-    }
 }
