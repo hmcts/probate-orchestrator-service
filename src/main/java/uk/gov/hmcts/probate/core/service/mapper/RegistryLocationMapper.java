0271b46b0d55aa1334780ee2669f5bfc4b6981ce
ruban72 <ruban72@hotmail.com>
2019-01-22 17:41:41 +0000

    PRI-139: updated ProbateCommons dependency

---
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/RegistryLocationMapper.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.probate.core.service.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.FromRegistryLocation;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.ToRegistryLocation;
+import uk.gov.hmcts.reform.probate.model.cases.RegistryLocation;
+
+@Component
+public class RegistryLocationMapper {
+
+
+    @FromRegistryLocation
+    public String fromRegistryLocation(RegistryLocation location){
+        return location.getName();
+    }
+
+    @ToRegistryLocation
+    public RegistryLocation toRegistryLocation(String strLocation){
+        return RegistryLocation.valueOf(strLocation.toUpperCase());
+    }
+}
