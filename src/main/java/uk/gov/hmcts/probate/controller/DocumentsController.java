0f78c7245a01552a74bd5a92d38b78fcc8902d76
Jake <jake.huggins@solirius.com>
2019-01-24 16:42:00 +0000

    PRI-142: add DocumentsController

---
+++ src/main/java/uk/gov/hmcts/probate/controller/DocumentsController.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.probate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.probate.service.BusinessService;
+import uk.gov.hmcts.reform.probate.model.documents.CheckAnswersSummary;
+
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/documents")
+public class DocumentsController {
+
+    private static final String CHECK_ANSWERS_ENDPOINT = "/generate/check-answers-summary";
+
+    private final BusinessService businessService;
+
+    @Autowired
+    private DocumentsController(BusinessService businessService) {
+        this.businessService = businessService;
+    }
+
+    @PostMapping(path = CHECK_ANSWERS_ENDPOINT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public ResponseEntity<byte[]> generateCheckAnswersSummaryPdf(@RequestBody CheckAnswersSummary checkAnswersSummary) {
+        log.info("generate pdf");
+        return new ResponseEntity<>(businessService.generateCheckAnswersSummaryPdf(checkAnswersSummary), HttpStatus.OK);
+    }
+
+}
