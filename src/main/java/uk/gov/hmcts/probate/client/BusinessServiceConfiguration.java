93fa45f394e666d007ce220497acbb53e2393107
Jake <jake.huggins@solirius.com>
2019-01-30 13:33:45 +0000

    PRI-142: remove primary annotation from business service config

--- src/main/java/uk/gov/hmcts/probate/client/BusinessServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/client/BusinessServiceConfiguration.java
@@ -6,14 +6,12 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import feign.codec.Decoder;
 import feign.jackson.JacksonDecoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 public class BusinessServiceConfiguration {
 
     static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
     @Bean
-    @Primary
     Decoder feignDecoder(ObjectMapper objectMapper) {
         objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
         objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
