8a9a5705b13ce94f0854134eb91b929b16139b3a
Jake <jake.huggins@solirius.com>
2019-02-01 16:15:20 +0000

    PRI-142: refactor businessServiceApi to return byte array

--- src/main/java/uk/gov/hmcts/probate/client/BusinessServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/client/BusinessServiceConfiguration.java
@@ -3,18 +3,19 @@ package uk.gov.hmcts.probate.client;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import feign.codec.Decoder;
-import feign.jackson.JacksonDecoder;
+import feign.codec.Encoder;
+import feign.jackson.JacksonEncoder;
 import org.springframework.context.annotation.Bean;
+
 
 public class BusinessServiceConfiguration {
 
     static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
     @Bean
-    Decoder feignDecoder(ObjectMapper objectMapper) {
+    Encoder feignEncoder(ObjectMapper objectMapper) {
         objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
         objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
-        return new JacksonDecoder(objectMapper);
+        return new JacksonEncoder(objectMapper);
     }
 }
