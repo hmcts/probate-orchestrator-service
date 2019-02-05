0d55c077357610919750c5eb345fb88da7812ca4
ruban72 <ruban72@hotmail.com>
2019-02-05 11:25:59 +0000

    PRI-142: Added LegalDec and BulkScan docs

--- src/main/java/uk/gov/hmcts/probate/controller/DocumentsController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/DocumentsController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.probate.service.BusinessService;
+import uk.gov.hmcts.reform.probate.model.documents.BulkScanCoverSheet;
 import uk.gov.hmcts.reform.probate.model.documents.CheckAnswersSummary;
+import uk.gov.hmcts.reform.probate.model.documents.LegalDeclaration;
 
 import javax.validation.Valid;
 
@@ -24,10 +26,13 @@ import javax.validation.Valid;
 @SwaggerDefinition(tags = {@Tag(name = "DocumentsController", description = "Documents API")})
 @Slf4j
 @RestController
-@RequestMapping(value = "/documents")
+@RequestMapping(value = DocumentsController.DOCUMENTS_BASEURL)
 public class DocumentsController {
 
-    private static final String CHECK_ANSWERS_ENDPOINT = "/generate/check-answers-summary";
+    protected static final String DOCUMENTS_BASEURL = "/documents";
+    protected static final String CHECK_ANSWERS_ENDPOINT = "/generate/checkAnswersSummary";
+    protected static final String LEGAL_DECLARATION_ENDPOINT = "/generate/legalDeclarationSummary";
+    protected static final String BULK_SCAN_COVERSHEET_ENDPOINT = "/generate/bulkScanCoversheet";
 
     private final BusinessService businessService;
 
@@ -43,6 +48,25 @@ public class DocumentsController {
     public ResponseEntity<byte[]> generateCheckAnswersSummaryPdf(@Valid @RequestBody CheckAnswersSummary checkAnswersSummary) {
         log.info("generate pdf");
         return new ResponseEntity<>(businessService.generateCheckAnswersSummaryPdf(checkAnswersSummary), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Generate PDF for Legal Declaration")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "PDF generated successfully")})
+    @PostMapping(path = LEGAL_DECLARATION_ENDPOINT, consumes = DocumentControllerConfiguration.APPLICATION_BUSINESSDOCUMENT_JSON)
+    public ResponseEntity<byte[]> generateLegalDeclarationPdf(@Valid @RequestBody LegalDeclaration legalDeclaration) {
+        log.info("generate pdf");
+        return new ResponseEntity<>(businessService.generateLegalDeclarationPdf(legalDeclaration), HttpStatus.OK);
+    }
+
+
+    @ApiOperation(value = "Generate PDF for Bulk Scan Coversheet")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "PDF generated successfully")})
+    @PostMapping(path = BULK_SCAN_COVERSHEET_ENDPOINT, consumes = DocumentControllerConfiguration.APPLICATION_BUSINESSDOCUMENT_JSON)
+    public ResponseEntity<byte[]> generateBulkScanCoversheet(@Valid @RequestBody BulkScanCoverSheet bulkScanCoverSheet) {
+        log.info("generate pdf");
+        return new ResponseEntity<>(businessService.generateBulkScanCoverSheetPdf(bulkScanCoverSheet), HttpStatus.OK);
     }
 
 }
