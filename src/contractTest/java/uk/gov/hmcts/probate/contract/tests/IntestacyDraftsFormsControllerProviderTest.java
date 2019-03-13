8d79af29c16a943f96b8382445f9eaf6ec28a407
ruban72 <ruban72@hotmail.com>
2019-03-13 16:41:33 +0000

    PRO-4814:pact changes

--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/IntestacyDraftsFormsControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/IntestacyDraftsFormsControllerProviderTest.java
@@ -8,18 +8,13 @@ import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.json.JSONException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.hmcts.probate.service.SubmitService;
-import uk.gov.hmcts.reform.probate.model.ProbateType;
-import uk.gov.hmcts.reform.probate.model.forms.intestacy.IntestacyApplicant;
-import uk.gov.hmcts.reform.probate.model.forms.intestacy.IntestacyDeceased;
-import uk.gov.hmcts.reform.probate.model.forms.intestacy.IntestacyDeclaration;
-import uk.gov.hmcts.reform.probate.model.forms.intestacy.IntestacyForm;
+import uk.gov.hmcts.probate.client.SubmitServiceApi;
+import uk.gov.hmcts.probate.core.service.SecurityUtils;
+import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
@@ -27,13 +22,15 @@ import static org.mockito.Mockito.when;
         "server.port=8889", "spring.application.name=PACT_TEST"
 })
 @Provider("probate_orchestrator_service_intestacy_forms")
-public class IntestacyDraftsFormsControllerProviderTest extends ControllerProviderTest{
+public class IntestacyDraftsFormsControllerProviderTest extends ControllerProviderTest {
 
     private static final LocalDate DATE_OF_BIRTH = LocalDate.of(1930, 01, 01);
     private static final LocalDate DATE_OF_DEATH = LocalDate.of(2018, 01, 01);
 
     @MockBean
-    private SubmitService submitService;
+    private SubmitServiceApi submitServiceApi;
+    @MockBean
+    private SecurityUtils securityUtils;
 
     @TestTarget
     @SuppressWarnings(value = "VisibilityModifier")
@@ -44,46 +41,16 @@ public class IntestacyDraftsFormsControllerProviderTest extends ControllerProvid
             "probate_orchestrator_service persists intestacy formdata with success"})
     public void toPersistIntestacyFormDataWithSuccess() throws IOException, JSONException {
 
-        IntestacyForm form = IntestacyForm.builder()
-                .type(ProbateType.INTESTACY)
-                .applicant(
-                        IntestacyApplicant.builder()
-                                .address("Pret a Manger St. Georges Hospital Blackshaw Road London SW17 0QT")
-                                .addressFound(Boolean.TRUE)
-                                .adoptionInEnglandOrWales(Boolean.TRUE)
-                                .email("someemailaddress@host.com")
-                                .firstName("Jon")
-                                .freeTextAddress("Pret a Manger St. Georges Hospital Blackshaw Road")
-                                .lastName("Snow")
-                                .phoneNumber("123455678")
-                                .postCode("SW17 0QT")
-                                .build())
-                .deceased(IntestacyDeceased.builder()
-                        .anyDeceasedChildrenDieBeforeDeceased(Boolean.FALSE)
-                        .address("Winterfell, Westeros")
-                        .addressFound(Boolean.TRUE)
-                        .alias(Boolean.TRUE)
-                        .allDeceasedChildrenOverEighteen(Boolean.TRUE)
-                        .anyChildren(Boolean.FALSE)
-                        .dateOfBirth(DATE_OF_BIRTH)
-                        .dateOfDeath(DATE_OF_DEATH)
-                        .divorcedInEnglandOrWales(Boolean.FALSE)
-                        .domiciledInEnglandOrWales(Boolean.TRUE)
-                        .firstName("Ned")
-                        .freeTextAddress("Winterfell, Westeros")
-                        .postCode("SW17 0QT")
-                        .lastName("Stark")
-                        .anyDeceasedGrandchildrenUnderEighteen(Boolean.FALSE)
-                        .build())
-                .declaration(IntestacyDeclaration.builder()
-                        .declarationAgreement(Boolean.TRUE)
-                        .build())
-                .build();
+        when(securityUtils.getAuthorisation()).thenReturn("someAuthorisationId");
+        when(securityUtils.getServiceAuthorisation()).thenReturn("someServiceAuthorisationId");
+
+        ProbateCaseDetails probateCaseDetails = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details.json");
+        ProbateCaseDetails probateCaseDetailsResponse = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details_response.json");
 
 
+        when(submitServiceApi.saveDraft("someAuthorisationId", "someServiceAuthorisationId", "someemailaddress@host.com", probateCaseDetails)).thenReturn(probateCaseDetailsResponse);
 
-        when(submitService.saveDraft(anyString(), any(IntestacyForm.class)))
-                .thenReturn(form);
     }
+
 
 }
--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
@@ -11,43 +11,42 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.probate.client.SubmitServiceApi;
 import uk.gov.hmcts.probate.core.service.SecurityUtils;
 import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
-import uk.gov.hmcts.reform.probate.model.cases.SubmitResult;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
 import static org.mockito.Mockito.when;
 
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
-        "server.port=8890", "spring.application.name=PACT_TEST"
+        "server.port=8889", "spring.application.name=PACT_TEST"
 })
-@Provider("probate_orchestrator_service_intestacy_submit")
-public class IntestacySubmissionsFormsControllerProviderTest extends ControllerProviderTest {
+@Provider("probate_orchestrator_service_probate_forms")
+public class ProbateDraftsFormsControllerProviderTest extends ControllerProviderTest{
 
     private static final LocalDate DATE_OF_BIRTH = LocalDate.of(1930, 01, 01);
     private static final LocalDate DATE_OF_DEATH = LocalDate.of(2018, 01, 01);
-
 
     @MockBean
     private SubmitServiceApi submitServiceApi;
     @MockBean
     private SecurityUtils securityUtils;
 
-
     @TestTarget
     @SuppressWarnings(value = "VisibilityModifier")
-    public final Target target = new HttpTarget("http", "localhost", 8890, "/");
+    public final Target target = new HttpTarget("http", "localhost", 8889, "/");
 
-    @State({"probate_orchestrator_service submits intestacy formdata with success",
-            "probate_orchestrator_service submits intestacy formdata with success"})
-    public void toPersistIntestacyFormDataWithSuccess() throws IOException, JSONException {
+
+    @State({"probate_orchestrator_service persists probate formdata with success",
+            "probate_orchestrator_service persists probate formdata with success"})
+    public void toPersistProbateFormDataWithSuccess() throws IOException, JSONException {
 
         when(securityUtils.getAuthorisation()).thenReturn("someAuthorisationId");
         when(securityUtils.getServiceAuthorisation()).thenReturn("someServiceAuthorisationId");
-        ProbateCaseDetails probateCaseDetails = getProbateCaseDetails("intestacyGrantOfRepresentation_submission.json");
-        ProbateCaseDetails probateCaseDetailsResponse = getProbateCaseDetails("intestacyGrantOfRepresentation_submission_response.json");
-        when(submitServiceApi.submit("someAuthorisationId", "someServiceAuthorisationId", "someemailaddress@host.com", probateCaseDetails)).thenReturn(new SubmitResult(probateCaseDetailsResponse, null));
-}
+        ProbateCaseDetails probateCaseDetails = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details.json");
+        ProbateCaseDetails probateCaseDetailsResponse = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details_response.json");
+        when(submitServiceApi.saveDraft("someAuthorisationId", "someServiceAuthorisationId", "someemailaddress@host.com", probateCaseDetails)).thenReturn(probateCaseDetailsResponse);
 
+    }
 
 }
