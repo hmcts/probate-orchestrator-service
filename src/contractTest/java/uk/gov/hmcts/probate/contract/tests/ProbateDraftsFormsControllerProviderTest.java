8cdb9b720c52d576186645b902e1b783ed61fe0c
ruban72 <ruban72@hotmail.com>
2019-03-22 12:51:27 +0000

    Added pact provider tests for Orchestrator

--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.probate.client.SubmitServiceApi;
 import uk.gov.hmcts.probate.core.service.SecurityUtils;
+import uk.gov.hmcts.reform.probate.model.cases.CaseType;
 import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
 
 import java.io.IOException;
@@ -44,5 +45,18 @@ public class ProbateDraftsFormsControllerProviderTest extends ControllerProvider
         when(submitServiceApi.saveDraft("someAuthorisationId", "someServiceAuthorisationId", "someemailaddress@host.com", probateCaseDetails)).thenReturn(probateCaseDetailsResponse);
 
     }
+
+    @State({"probate_orchestrator_service gets formdata with success",
+            "probate_orchestrator_service gets formdata with success"})
+    public void toGetProbateFormDataWithSuccess() throws IOException, JSONException {
+
+        when(securityUtils.getAuthorisation()).thenReturn("someAuthorisationId");
+        when(securityUtils.getServiceAuthorisation()).thenReturn("someServiceAuthorisationId");
+        ProbateCaseDetails probateCaseDetails = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details.json");
+        ProbateCaseDetails probateCaseDetailsResponse = getProbateCaseDetails("probate_orchestrator_service_persists_intestacy_formdata_with_success_probate_case_details_response.json");
+        when(submitServiceApi.getCase("someAuthorisationId", "someServiceAuthorisationId", "someemailaddress@host.com", CaseType.GRANT_OF_REPRESENTATION.getName())).thenReturn(probateCaseDetailsResponse);
+
+    }
+
 
 }
