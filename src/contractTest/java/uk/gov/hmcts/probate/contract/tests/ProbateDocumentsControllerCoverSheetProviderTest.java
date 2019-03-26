d5e3b8cddf66f721052ce27db17b0b1610831b65
ruban72 <ruban72@hotmail.com>
2019-03-26 10:11:17 +0000

    PRO-4814: Use random ports for provider test

--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDocumentsControllerCoverSheetProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDocumentsControllerCoverSheetProviderTest.java
@@ -2,11 +2,7 @@ package uk.gov.hmcts.probate.contract.tests;
 
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.json.JSONException;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.probate.client.BusinessServiceApi;
 import uk.gov.hmcts.probate.core.service.SecurityUtils;
@@ -19,22 +15,13 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
-        "server.port=8889", "spring.application.name=PACT_TEST"
-})
 @Provider("probate_orchestrator_service_documents_cover_sheet")
 public class ProbateDocumentsControllerCoverSheetProviderTest extends ControllerProviderTest{
-
 
     @MockBean
     private BusinessServiceApi businessServiceApi;
     @MockBean
     private SecurityUtils securityUtils;
-
-    @TestTarget
-    @SuppressWarnings(value = "VisibilityModifier")
-    public final Target target = new HttpTarget("http", "localhost", 8889, "/");
-
 
     @State({"probate_orchestrator_service generates cover sheet byte[] with success",
             "probate_orchestrator_service generates cover sheet byte[] with success"})
--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ProbateDraftsFormsControllerProviderTest.java
@@ -2,11 +2,7 @@ package uk.gov.hmcts.probate.contract.tests;
 
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.json.JSONException;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.probate.client.SubmitServiceApi;
 import uk.gov.hmcts.probate.core.service.SecurityUtils;
@@ -18,9 +14,6 @@ import java.io.IOException;
 import static org.mockito.Mockito.when;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {
-        "server.port=8889", "spring.application.name=PACT_TEST"
-})
 @Provider("probate_orchestrator_service_probate_forms")
 public class ProbateDraftsFormsControllerProviderTest extends ControllerProviderTest{
 
@@ -28,11 +21,6 @@ public class ProbateDraftsFormsControllerProviderTest extends ControllerProvider
     private SubmitServiceApi submitServiceApi;
     @MockBean
     private SecurityUtils securityUtils;
-
-    @TestTarget
-    @SuppressWarnings(value = "VisibilityModifier")
-    public final Target target = new HttpTarget("http", "localhost", 8889, "/");
-
 
     @State({"probate_orchestrator_service persists probate formdata with success",
             "probate_orchestrator_service persists probate formdata with success"})
