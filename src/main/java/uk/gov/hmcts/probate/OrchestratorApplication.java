e1ec6a1cb7702d4e3dbfc14a52fd578e8eaacef9
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2019-05-20 16:53:16 +0100

    PRO-4080: added logging

--- src/main/java/uk/gov/hmcts/probate/OrchestratorApplication.java
+++ src/main/java/uk/gov/hmcts/probate/OrchestratorApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
 
 @EnableSwagger2
 @EnableFeignClients
-@EnableAsync
-@SpringBootApplication(exclude= {ServiceAuthHealthIndicator.class})
+//@EnableAsync
+@SpringBootApplication(exclude = {ServiceAuthHealthIndicator.class})
 public class OrchestratorApplication {
 
     @Bean
