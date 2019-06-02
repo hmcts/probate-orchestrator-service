e9016032870ac85bdf2e820981c08da7e5e0149e
Dhruv Patel <dhruv.patel20@gmail.com>
2019-06-03 00:43:19 +0100

    PRO-5248: Added complete payment logic in the backend.

--- src/main/java/uk/gov/hmcts/probate/service/PaymentService.java
+++ src/main/java/uk/gov/hmcts/probate/service/PaymentService.java
@@ -1,15 +1,13 @@
 package uk.gov.hmcts.probate.service;
 
+import uk.gov.hmcts.reform.probate.model.ProbateType;
 import uk.gov.hmcts.reform.probate.model.forms.Form;
-import uk.gov.hmcts.reform.probate.model.payments.PaymentDto;
-
-import java.util.Optional;
+import uk.gov.hmcts.reform.probate.model.forms.PaymentSubmission;
 
 public interface PaymentService {
 
-    PaymentDto createPayment(Form form, String returnUrl);
+    PaymentSubmission createPaymentSubmission(String identifier, ProbateType probateType, String returnUrl, String callbackUrl);
 
-
-    Optional<PaymentDto> findNonFailedPaymentByCaseId(String caseId);
+    Form updatePaymentSubmission(String identifier, ProbateType probateType);
 
 }
