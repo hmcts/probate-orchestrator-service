83834b4f662e6a7d43d0c06e675dd01c6ba58f9c
ruban72 <ruban72@hotmail.com>
2019-06-10 12:44:31 +0100

    PRO-5237: Executor address fixed

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/ExecutorApplyingMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/ExecutorApplyingMapper.java
@@ -5,6 +5,8 @@ import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.ToCaseAddress;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.ToFormAddress;
 import uk.gov.hmcts.reform.probate.model.cases.CollectionMember;
 import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.ExecutorApplying;
 import uk.gov.hmcts.reform.probate.model.forms.pa.Executor;
@@ -15,7 +17,7 @@ public interface ExecutorApplyingMapper {
     @Mapping(target = "value.applyingExecutorName", expression = "java(ExecutorNamesMapper.getFullname(executor))")
     @Mapping(target = "value.applyingExecutorPhoneNumber", source = "mobile")
     @Mapping(target = "value.applyingExecutorEmail", source = "email")
-    @Mapping(target = "value.applyingExecutorAddress.addressLine1", source = "address")
+    @Mapping(target = "value.applyingExecutorAddress", source = "address", qualifiedBy = {ToCaseAddress.class})
     @Mapping(target = "value.applyingExecutorOtherNames", expression = "java(BooleanUtils.isTrue(executor.getHasOtherName()) ? executor.getCurrentName() : null)")
     @Mapping(target = "value.applyingExecutorOtherNamesReason", expression = "java(BooleanUtils.isTrue(executor.getHasOtherName()) ? executor.getCurrentNameReason() : null)")
     @Mapping(target = "value.applyingExecutorOtherReason", expression = "java(BooleanUtils.isTrue(executor.getHasOtherName()) ? executor.getOtherReason() : null)")
@@ -28,6 +30,7 @@ public interface ExecutorApplyingMapper {
     @Mapping(target = "hasOtherName", expression = "java(executorApplyingCollectionMember.getValue().getApplyingExecutorOtherNames() != null)")
     @Mapping(target = "currentName", source = "value.applyingExecutorOtherNames")
     @Mapping(target = "currentNameReason", source = "value.applyingExecutorOtherNamesReason")
+    @Mapping(target = "address", source = "value.applyingExecutorAddress", qualifiedBy = {ToFormAddress.class})
     @Mapping(target = "otherReason", source = "value.applyingExecutorOtherReason")
     @Mapping(target = "isApplying", expression = "java(true)")
     @Mapping(target = "fullName", source = "value.applyingExecutorName")
