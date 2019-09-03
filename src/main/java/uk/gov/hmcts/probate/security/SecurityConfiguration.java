d5ee11d0409b3f2b53fe19d9a7bd9543071a3846
ruban72 <ruban72@hotmail.com>
2019-09-03 13:50:34 +0100

    PRO-5247: Pin page whitelisted

--- src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 "/health/liveness",
                 "/info",
                 "/invite/data/**",
+                "/invite/pin/**",
                 "/favicon.ico",
                 "/");
     }
