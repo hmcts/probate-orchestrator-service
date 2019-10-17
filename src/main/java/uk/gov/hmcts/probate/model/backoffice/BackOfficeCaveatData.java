cd3157a0ee7502e88fba24a58b64e9ac312fa19d
ruban72 <ruban72@hotmail.com>
2019-10-17 21:43:54 +0100

    PRO-6212: Fix default constructors

--- src/main/java/uk/gov/hmcts/probate/model/backoffice/BackOfficeCaveatData.java
+++ src/main/java/uk/gov/hmcts/probate/model/backoffice/BackOfficeCaveatData.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.probate.model.backoffice;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.probate.model.ProbateDocument;
 import uk.gov.hmcts.reform.probate.model.cases.Address;
 import uk.gov.hmcts.reform.probate.model.cases.CollectionMember;
@@ -11,32 +13,34 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BackOfficeCaveatData {
 
-    private final String registryLocation;
+    private String registryLocation;
 
-    private final String deceasedForenames;
-    private final String deceasedSurname;
-    private final String deceasedDateOfDeath;
-    private final String deceasedDateOfBirth;
-    private final String deceasedAnyOtherNames;
-    private final List<CollectionMember<FullAliasName>> deceasedFullAliasNameList;
-    private final Address deceasedAddress;
+    private String deceasedForenames;
+    private String deceasedSurname;
+    private String deceasedDateOfDeath;
+    private String deceasedDateOfBirth;
+    private String deceasedAnyOtherNames;
+    private List<CollectionMember<FullAliasName>> deceasedFullAliasNameList;
+    private Address deceasedAddress;
 
-    private final String caveatorForenames;
-    private final String caveatorSurname;
-    private final String caveatorEmailAddress;
-    private final Address caveatorAddress;
+    private String caveatorForenames;
+    private String caveatorSurname;
+    private String caveatorEmailAddress;
+    private Address caveatorAddress;
 
-    private final String expiryDate;
-    private final String applicationSubmittedDate;
-    private final String messageContent;
-    private final String caveatReopenReason;
+    private String expiryDate;
+    private String applicationSubmittedDate;
+    private String messageContent;
+    private String caveatReopenReason;
 
-    private final String caveatRaisedEmailNotificationRequested;
-    private final String caveatRaisedEmailNotification;
+    private String caveatRaisedEmailNotificationRequested;
+    private String caveatRaisedEmailNotification;
 
-    private final List<CollectionMember<ProbateDocument>> notificationsGenerated;
+    private List<CollectionMember<ProbateDocument>> notificationsGenerated;
 
     private String recordId;
     private String legacyType;
--- src/main/java/uk/gov/hmcts/probate/model/backoffice/BackOfficeCaveatResponse.java
+++ src/main/java/uk/gov/hmcts/probate/model/backoffice/BackOfficeCaveatResponse.java
@@ -13,5 +13,5 @@ import lombok.NoArgsConstructor;
 public class BackOfficeCaveatResponse {
 
     @JsonProperty(value = "data")
-    private  BackOfficeCaveatData caseData;
+    private BackOfficeCaveatData caseData;
 }
