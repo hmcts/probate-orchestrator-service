09ad160e555a2f043e284931a218f34c338c5320
ruban72 <ruban72@hotmail.com>
2019-11-11 13:54:20 +0000

    PRO-6548: remove camelcase from postcode for postcode lookup

--- src/test/java/uk/gov/hmcts/probate/core/service/migration/IdamUsersCsvLoaderTest.java
+++ src/test/java/uk/gov/hmcts/probate/core/service/migration/IdamUsersCsvLoaderTest.java
@@ -4,15 +4,14 @@ import org.hamcrest.core.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.List;
 
 public class IdamUsersCsvLoaderTest {
 
-    IdamUsersCsvLoader csvLoader = new IdamUsersCsvLoader();
+    private IdamUsersCsvLoader csvLoader = new IdamUsersCsvLoader();
 
     @Test
-    public void shouldLoadCsvFile() throws IOException {
+    public void shouldLoadCsvFile() {
         List<IdamUserEmail> idamUserEmails = csvLoader.loadIdamUserList("idam_ids.csv");
         Assert.assertThat(idamUserEmails.size(), Is.is(5));
     }
