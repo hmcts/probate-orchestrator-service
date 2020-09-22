85d25637521fdc166583c00588c1f4e2ca7b8ed9
KleoG <kleogeorgiou@hotmail.com>
2020-09-22 09:37:14 +0100

    Added Service only authorisation to SecurityConfiguration Refactor SecurityConfiguration Add payment_app as an authorised service

--- src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.probate.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.AuthCheckerServiceAndUserFilter;
+import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -18,54 +19,113 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
+    @Configuration
+    @Order(1)
+    public static class AuthCheckerServiceAndUserFilterConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-    private AuthenticationExceptionHandler authenticationExceptionHandler;
+        private final AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
 
-    @Autowired
-    public SecurityConfiguration(RequestAuthorizer<User> userRequestAuthorizer,
-                                 RequestAuthorizer<Service> serviceRequestAuthorizer,
-                                 AuthenticationManager authenticationManager,
-                                 AuthenticationExceptionHandler authenticationExceptionHandler) {
-        authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
-        authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
-        this.authenticationExceptionHandler = authenticationExceptionHandler;
+        private AuthenticationExceptionHandler authenticationExceptionHandler;
+
+        public AuthCheckerServiceAndUserFilterConfigurerAdapter(RequestAuthorizer<User> userRequestAuthorizer,
+                                                                RequestAuthorizer<Service> serviceRequestAuthorizer,
+                                                                AuthenticationManager authenticationManager,
+                                                                AuthenticationExceptionHandler authenticationExceptionHandler) {
+            authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
+            authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
+            this.authenticationExceptionHandler = authenticationExceptionHandler;
+        }
+
+
+
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers("/swagger-ui.html",
+                    "/webjars/springfox-swagger-ui/**",
+                    "/swagger-resources/**",
+                    "/v2/**",
+                    "/health",
+                    "/health/liveness",
+                    "/info",
+                    "/migrateData",
+                    "/favicon.ico",
+                    "/");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .requestMatchers()
+                    .antMatchers("/documents/**")
+                    .antMatchers("/generate/**")
+                    .antMatchers("/forms/**")
+                    .antMatchers("/invite/**")
+                    .antMatchers("/invites/**")
+                    .and()
+                    .addFilter(authCheckerServiceAndUserFilter)
+                    .sessionManagement().sessionCreationPolicy(STATELESS).and()
+                    .csrf().disable()
+                    .formLogin().disable()
+                    .logout().disable()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(authenticationExceptionHandler);
+        }
+
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html",
-                "/webjars/springfox-swagger-ui/**",
-                "/swagger-resources/**",
-                "/v2/**",
-                "/health",
-                "/health/liveness",
-                "/info",
-                "/migrateData",
-                "/favicon.ico",
-                "/");
+
+    @Configuration
+    public static class AuthCheckerServiceOnlyFilterConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        private AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter;
+
+        private AuthenticationExceptionHandler authenticationExceptionHandler;
+
+        public AuthCheckerServiceOnlyFilterConfigurerAdapter(RequestAuthorizer<Service> serviceRequestAuthorizer,
+                                                             AuthenticationManager authenticationManager,
+                                                             AuthenticationExceptionHandler authenticationExceptionHandler) {
+            authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(serviceRequestAuthorizer);
+            authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
+            this.authenticationExceptionHandler = authenticationExceptionHandler;
+        }
+
+        @Override
+        @Order(2)
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http
+                    .requestMatchers()
+                    .antMatchers("/payment-updates")
+                    .and()
+                    .addFilter(authCheckerServiceOnlyFilter)
+                    .sessionManagement().sessionCreationPolicy(STATELESS).and()
+                    .csrf().disable()
+                    .formLogin().disable()
+                    .logout().disable()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(authenticationExceptionHandler);
+
+        }
+
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers("/swagger-ui.html",
+                    "/swagger-resources/**",
+                    "/webjars/springfox-swagger-ui/**",
+                    "/v2/api-docs",
+                    "/health",
+                    "/health/liveness",
+                    "/info",
+                    "/data-extract/**",
+                    "/");
+        }
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .requestMatchers()
-                .antMatchers("/documents/**")
-                .antMatchers("/generate/**")
-                .antMatchers("/forms/**")
-                .antMatchers("/invite/**")
-                .antMatchers("/invites/**")
-                .and()
-                .addFilter(authCheckerServiceAndUserFilter)
-                .sessionManagement().sessionCreationPolicy(STATELESS).and()
-                .csrf().disable()
-                .formLogin().disable()
-                .logout().disable()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationExceptionHandler);
-    }
 }
