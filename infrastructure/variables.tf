a770362c4130b6899808559250f747225e94dbf1
Linus Norton <linusnorton@gmail.com>
2020-10-02 10:18:15 +0100

    Add common tags

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -20,3 +20,7 @@ variable "subscription" {}
 variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
+
+variable "common_tags" {
+  type = map(string)
+}
