f795213a4b76dd03097affbcaa90e2ff5e08bf5e
stelios <stelios.kalorkotis@hotmail.com>
2020-10-27 09:58:58 +0000

    PrimaryApplicantHasAlias mapping has been added to PaMapper.

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/PaMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/PaMapper.java
@@ -68,6 +68,7 @@ public interface PaMapper extends FormMapper<GrantOfRepresentationData, PaForm> 
     @Mapping(target = "primaryApplicantSurname", source = "applicant.lastName")
     @Mapping(target = "primaryApplicantEmailAddress", source = "applicantEmail")
     @Mapping(target = "primaryApplicantAlias", source = "applicant.alias")
+    @Mapping(target = "primaryApplicantHasAlias", source = "applicant.nameAsOnTheWill")
     @Mapping(target = "primaryApplicantAliasReason", expression = "java(form.getApplicant()!= null && form.getApplicant().getAliasReason() != null ? AliasReason.fromString(form.getApplicant().getAliasReason()) : null)")
     @Mapping(target = "primaryApplicantAddress", source = "applicant.address", qualifiedBy = {ToCaseAddress.class})
     @Mapping(target = "primaryApplicantPostCode", source = "applicant.postcode")
