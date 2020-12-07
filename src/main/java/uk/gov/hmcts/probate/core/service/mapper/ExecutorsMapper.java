97c785d74afe76daeb31f16ef7f8f7ad698fa7f9
itoulou <touloumbadjiani@gmail.com>
2020-12-07 15:15:26 +0000

    extra info in logs

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/ExecutorsMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/ExecutorsMapper.java
@@ -48,18 +48,10 @@ public class ExecutorsMapper {
         log.info("IN HERE!!");
         for(int i = 0; i < executors.size(); i++){
             if(executors.get(i).getEmailChanged() != null) {
-                log.info("");
-                log.info("=====================");
                 log.info("emailChanged= " + executors.get(i).getEmailChanged().toString());
-                log.info("=====================");
-                log.info("");
             }
             if(executors.get(i).getEmailSent() != null) {
-                log.info("");
-                log.info("=====================");
                 log.info("emailChanged= " + executors.get(i).getEmailSent().toString());
-                log.info("=====================");
-                log.info("");
             }
         }
         log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
