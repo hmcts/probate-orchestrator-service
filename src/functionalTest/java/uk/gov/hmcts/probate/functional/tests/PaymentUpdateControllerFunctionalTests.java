393f9c48eda3c21c63325634972b3daea42c3965
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-03-08 21:21:32 +0000

    DTSPB-1467 change the success functional test for payment

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/tests/PaymentUpdateControllerFunctionalTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/tests/PaymentUpdateControllerFunctionalTests.java
@@ -4,26 +4,35 @@ import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import uk.gov.hmcts.probate.functional.IntegrationTestBase;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 public class PaymentUpdateControllerFunctionalTests extends IntegrationTestBase {
+    public static final String SERVICE_AUTHORIZATION_HEADER = "ServiceAuthorization";
+    public static final String BEARER_AUTH_TOKEN = "Bearer test.auth.token1";
     private static final String PAYMENT_UPDATE_URL = "/payment-updates";
 
-    private static final String s2sToken = "auth Token";
+    @Autowired
+    private MockMvc webClient;
 
     @Test
-    public void updatePaymentDetails() {
+    public void updatePaymentDetails() throws Exception {
         String draftJsonStr = utils.getJsonFromFile("payment.json");
-        RestAssured.given()
-            .relaxedHTTPSValidation()
-            .headers("ServiceAuthorization", s2sToken)
-            .body(draftJsonStr)
-            .when()
-            .put(PAYMENT_UPDATE_URL)
-            .then()
-            .assertThat()
-            .statusCode(200);
+
+        webClient.perform(put(PAYMENT_UPDATE_URL)
+            .header(SERVICE_AUTHORIZATION_HEADER, BEARER_AUTH_TOKEN)
+            .content(draftJsonStr)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().is2xxSuccessful());
     }
 
     @Test
