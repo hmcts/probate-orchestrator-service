0b006fc021edd9fce70d4f6d6076114dc7a3a1dd
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-03-08 20:27:41 +0000

    commit to rebuild

--- src/main/java/uk/gov/hmcts/probate/controller/PaymentUpdateController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/PaymentUpdateController.java
@@ -46,9 +46,9 @@ public class PaymentUpdateController {
         @ApiResponse(code = 500, message = "Internal Server Error")})
     @PutMapping(path = PAYMENT_UPDATES, consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE)
-    //@ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<HttpStatus> updatePayment(@RequestHeader(value = SERVICE_AUTHORIZATION_HEADER) String s2sAuthToken,
-                                           @RequestBody PaymentDto paymentDto) {
+    public ResponseEntity<HttpStatus> updatePayment(
+        @RequestHeader(value = SERVICE_AUTHORIZATION_HEADER) String s2sAuthToken,
+        @RequestBody PaymentDto paymentDto) {
 
         try {
             Boolean isServiceAllowed = authS2sUtil.checkIfServiceIsAllowed(s2sAuthToken);
