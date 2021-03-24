a46a55445dfa9a197f9eb897b43ada33614a13ce
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-03-24 11:13:14 +0000

    DTSPB-1296: added UTs2

--- src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerUnitTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerUnitTest.java
@@ -12,8 +12,6 @@ import uk.gov.hmcts.probate.service.DataExtractService;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
