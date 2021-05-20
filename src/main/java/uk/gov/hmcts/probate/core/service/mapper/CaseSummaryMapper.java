06f603b78e9c30f18f80c0aba266a42b9b19153e
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-05-20 15:44:52 +0100

    DTSPB-1946: remove null pointer from summary mapper2

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/CaseSummaryMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/CaseSummaryMapper.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.probate.core.service.mapper;
 
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.probate.model.ProbateType;
 import uk.gov.hmcts.reform.probate.model.cases.ProbateCaseDetails;
 import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantOfRepresentationData;
-import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantType;
 import uk.gov.hmcts.reform.probate.model.forms.CaseSummary;
 import uk.gov.hmcts.reform.probate.model.forms.CcdCase;
 
@@ -18,13 +16,13 @@ public class CaseSummaryMapper {
     public CaseSummary createCaseSummary(ProbateCaseDetails caseDetails) {
         GrantOfRepresentationData gop = (GrantOfRepresentationData) caseDetails.getCaseData();
         return CaseSummary.builder().dateCreated(caseDetails.getCaseInfo().getCaseCreatedDate())
-                .deceasedFullName(formatDeceasedFullName(gop))
-                .caseType(GRANT_OF_PROBATE.equals(gop.getGrantType()) 
-                    ? PA.name() : INTESTACY.name())
-                .ccdCase(CcdCase.builder()
-                        .state(caseDetails.getCaseInfo().getState().getName())
-                        .id(Long.parseLong(caseDetails.getCaseInfo().getCaseId())).build())
-                .build();
+            .deceasedFullName(formatDeceasedFullName(gop))
+            .caseType(GRANT_OF_PROBATE.equals(gop.getGrantType())
+                ? PA.name() : INTESTACY.name())
+            .ccdCase(CcdCase.builder()
+                .state(caseDetails.getCaseInfo().getState().getName())
+                .id(Long.parseLong(caseDetails.getCaseInfo().getCaseId())).build())
+            .build();
     }
 
     private String formatDeceasedFullName(GrantOfRepresentationData gop) {
