5e4ed653445680409ceb9ef4929812711f61bfdf
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-05-20 15:15:22 +0100

    DTSPB-1946: remove null pointer from summary mapper

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/CaseSummaryMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/CaseSummaryMapper.java
@@ -8,6 +8,10 @@ import uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantType;
 import uk.gov.hmcts.reform.probate.model.forms.CaseSummary;
 import uk.gov.hmcts.reform.probate.model.forms.CcdCase;
 
+import static uk.gov.hmcts.reform.probate.model.ProbateType.INTESTACY;
+import static uk.gov.hmcts.reform.probate.model.ProbateType.PA;
+import static uk.gov.hmcts.reform.probate.model.cases.grantofrepresentation.GrantType.GRANT_OF_PROBATE;
+
 @Component
 public class CaseSummaryMapper {
 
@@ -15,8 +19,8 @@ public class CaseSummaryMapper {
         GrantOfRepresentationData gop = (GrantOfRepresentationData) caseDetails.getCaseData();
         return CaseSummary.builder().dateCreated(caseDetails.getCaseInfo().getCaseCreatedDate())
                 .deceasedFullName(formatDeceasedFullName(gop))
-                .caseType(gop.getGrantType()
-                        .equals(GrantType.GRANT_OF_PROBATE) ? ProbateType.PA.name() : ProbateType.INTESTACY.name())
+                .caseType(GRANT_OF_PROBATE.equals(gop.getGrantType()) 
+                    ? PA.name() : INTESTACY.name())
                 .ccdCase(CcdCase.builder()
                         .state(caseDetails.getCaseInfo().getState().getName())
                         .id(Long.parseLong(caseDetails.getCaseInfo().getCaseId())).build())
