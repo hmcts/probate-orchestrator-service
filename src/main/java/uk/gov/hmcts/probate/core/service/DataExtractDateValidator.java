87cb6a34550e3e0423be62eed5c704fb9e69169a
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-07-27 16:13:58 +0100

    DTSPB-1867: spring boot upgrade

--- src/main/java/uk/gov/hmcts/probate/core/service/DataExtractDateValidator.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/DataExtractDateValidator.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.probate.core.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.probate.model.client.ApiClientError;
