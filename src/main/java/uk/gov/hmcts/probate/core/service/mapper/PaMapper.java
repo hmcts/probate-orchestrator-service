710c73d637928c58e907a90e4f289845833347f5
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-10-20 17:48:42 +0100

    Adding one more check for appicant.AliasReason

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/PaMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/PaMapper.java
@@ -183,7 +183,8 @@ public interface PaMapper extends FormMapper<GrantOfRepresentationData, PaForm> 
         expression = "java(grantOfRepresentationData.getOutsideUkGrantCopies() == null ? "
             + "null : grantOfRepresentationData.getOutsideUkGrantCopies() > 0L)")
     @Mapping(target = "applicant.aliasReason",
-        expression = "java(grantOfRepresentationData.getPrimaryApplicantAliasReason()!=null ? "
+        expression = "java(grantOfRepresentationData.getPrimaryApplicantAliasReason()!=null "
+            + "&& !grantOfRepresentationData.getPrimaryApplicantAliasReason().isEmpty() ? "
             + "grantOfRepresentationData.getPrimaryApplicantAliasReason().getDescription() : null)")
     @Mapping(target = "applicant.address", source = "primaryApplicantAddress", qualifiedBy = {ToFormAddress.class})
     @Mapping(target = "applicant.addresses", source = "primaryApplicantAddresses", qualifiedBy = {ToMap.class})
