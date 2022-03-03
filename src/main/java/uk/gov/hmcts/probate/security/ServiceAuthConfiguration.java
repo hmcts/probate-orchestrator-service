246e846156ebae82be9a1294051f86ef9f2207fc
damongreen123 <damon.green@btinternet.com>
2022-03-03 11:36:01 +0000

    Update ServiceAuthConfiguration.java

--- src/main/java/uk/gov/hmcts/probate/security/ServiceAuthConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/ServiceAuthConfiguration.java
@@ -19,7 +19,7 @@ public class ServiceAuthConfiguration {
     @Bean
     public ServiceAuthTokenGenerator serviceAuthTokenGenerator(@Value("${service.auth.provider.base.url}")
                                                                        String s2sUrl,
-                                                               @Value("${s2s.auth.totp.secret}") String secret,
+                                                               @Value("${s2s7.auth.totp.secret}") String secret,
                                                                @Value("${service.name}") String microservice) {
         final ServiceAuthorisationApi serviceAuthorisationApi = Feign.builder()
                 .encoder(new JacksonEncoder())
