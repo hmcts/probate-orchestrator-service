2716f7c682f3ca6319d984b61f0cd630ac07c883
isha2040 <58481974+isha2040@users.noreply.github.com>
2022-03-28 21:05:56 +0100

    Dtspb 2613 upgrade spring boot (#662)
    * DTSPB-2613: Upgrade Springboot version for probate orchestrator Service
    
    * DTSPB-2613: Upgrade Springboot version for probate orchestrator Service
    
    * DTSPB-2613: Upgrade Springboot version for probate orchestrator Service
    
    * DTSPB-2613: Upgrade Springboot version for probate orchestrator Service
    
    * Rewriting the dependency set
    
    * DTSPB-2613 Fix the failing test
    
    * Point to other repos for SB related changes
    
    * Bumping chart version
    
    * Remove the Jackson related transitive dependency
    
    * Bumping chart version
    
    * Update the Jackson Data bind version
    
    * Suppress the jackson databind core related cve temporarily
    
    * Point to the new PRs in orch
    
    * Revert the hardcoded pointing of PRs
    
    Co-authored-by: sunilrochani <sunil.rochani@hotmail.com>
    Co-authored-by: hmcts-jenkins-cft <93778408+hmcts-jenkins-cft[bot]@users.noreply.github.com>
    Co-authored-by: Jenkins <nobody@nowhere>

--- src/main/java/uk/gov/hmcts/probate/configuration/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/configuration/SwaggerConfiguration.java
@@ -1,58 +1,36 @@
 package uk.gov.hmcts.probate.configuration;
 
-import com.google.common.base.Predicate;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket orchestratorApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("orchestratorApi")
-                .globalOperationParameters(Arrays.asList(
-                        new ParameterBuilder()
-                                .name("Authorization")
+    public OpenAPI orchestratorApi() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addHeaders("Authorization",
+                                new Header()
                                 .description("User authorization header")
                                 .required(true)
-                                .parameterType("header")
-                                .modelRef(new ModelRef("string"))
-                                .build(),
-                        new ParameterBuilder()
-                                .name("ServiceAuthorization")
+                                .schema(new StringSchema()))
+                        .addHeaders("ServiceAuthorization",
+                                new Header()
                                 .description("Service authorization header")
                                 .required(true)
-                                .parameterType("header")
-                                .modelRef(new ModelRef("string"))
-                                .build())
-                )
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(packagesLike("uk.gov.hmcts.probate.controller"))
-                .paths(PathSelectors.any())
-                .build();
+                                .schema(new StringSchema())))
+                .info(apiInfo());
     }
 
-    private static Predicate<RequestHandler> packagesLike(final String pkg) {
-        return input -> input.declaringClass().getPackage().getName().equals(pkg);
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    private Info apiInfo() {
+        return new Info()
                 .title("Probate Orchestrator API Documentation")
-                .description("Probate Orchestrator API Documentation")
-                .build();
+                .description("Probate Orchestrator API Documentation");
     }
 }
