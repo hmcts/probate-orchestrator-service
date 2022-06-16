e20ca40771b70b2f6f7e45e3202ba5077a9e6a19
sunilrochani <sunil.rochani@hotmail.com>
2022-06-16 09:37:52 +0100

    Revert DTSPB-2824: Investigation: Effective way for Scheduling

--- src/main/java/uk/gov/hmcts/probate/OrchestratorApplication.java
+++ src/main/java/uk/gov/hmcts/probate/OrchestratorApplication.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.probate;
 
 import feign.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,45 +8,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.gov.hmcts.probate.client.FeignErrorDecoder;
-import uk.gov.hmcts.probate.core.service.task.ScheduledTaskRunner;
-
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @EnableFeignClients
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
-public class OrchestratorApplication implements CommandLineRunner {
-
-    @Autowired
-    ScheduledTaskRunner taskRunner;
-
-    public static void main(final String[] args) {
-        final var application = new SpringApplication(OrchestratorApplication.class);
-        final var instance = application.run(args);
-
-        if (System.getenv("TASK_NAME") != null) {
-            instance.close();
-        }
-    }
-
-    @Override
-    public void run(String... args) {
-        if (System.getenv("TASK_NAME") != null) {
-            taskRunner.run(System.getenv("TASK_NAME"));
-        }
-    }
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-    }
+public class OrchestratorApplication {
 
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestratorApplication.class, args);
+    }
+
 
     @Bean
     public FeignErrorDecoder errorDecoder() {
