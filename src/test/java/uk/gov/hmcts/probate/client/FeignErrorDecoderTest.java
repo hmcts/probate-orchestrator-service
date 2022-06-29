2111cde157442b06446723d02e557e5c05369bb4
Iswarya Pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2022-06-29 11:09:21 +0100

    Deleted unused files and added tests

---
+++ src/test/java/uk/gov/hmcts/probate/client/FeignErrorDecoderTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.probate.client;
+
+import feign.Request;
+import feign.Response;
+import feign.Util;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FeignErrorDecoderTest {
+    private Map<String, Collection<String>> headers = new LinkedHashMap<>();
+    private FeignErrorDecoder decoder = new FeignErrorDecoder();
+
+    @Test
+    void throwsException() {
+        Response response = Response.builder()
+                .status(500)
+                .reason("Internal server error")
+                .request(Request.create(Request.HttpMethod.GET,
+                        "/api", Collections.emptyMap(), null, Util.UTF_8, null))
+                .headers(headers)
+                .build();
+
+        assertThrows(Exception.class, () -> {
+            throw decoder.decode("Service#foo()", response);
+        });
+    }
+}
