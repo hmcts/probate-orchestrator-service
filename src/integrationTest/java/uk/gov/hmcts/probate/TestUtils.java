2e957e71cb1728831130cd3246b36629c907bfa0
David Gimelle <davidgimelle1@hmcts.net>
2023-01-23 10:59:10 +0000

    Checkstyle

--- src/integrationTest/java/uk/gov/hmcts/probate/TestUtils.java
+++ src/integrationTest/java/uk/gov/hmcts/probate/TestUtils.java
@@ -1,20 +1,16 @@
 package uk.gov.hmcts.probate;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class TestUtils {
 
     public static String getJsonFromFile(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get("src/test/resources", fileName)));
+    }
+
+    private TestUtils(){
     }
 
 }
