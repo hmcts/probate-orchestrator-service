daf156bb018780e7b687ff3cacce229af1492fb9
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-03-21 18:42:41 +0000

    DTSPB-3621 OpenId Connect Token

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/TestTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/TestTokenGenerator.java
@@ -90,10 +90,9 @@ public class TestTokenGenerator {
                         + "&redirect_uri=" + redirectUri
                         + "&username=" + email
                         + "&password=" + password
-                        + "&grant_type=password&scope=openid")
+                        + "&grant_type=password&scope=openid profile roles")
                 .body().jsonPath();
         String token = jp.get("access_token");
-
         return token;
     }
 }
