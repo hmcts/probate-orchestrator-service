8679ef3171229ce2a7af76523d2770fb2540775e
Michael1142 <michael.johnson9@justice.gov.uk>
2024-05-17 17:19:46 +0100

    Update test as 4 digit error codes are no longer valid

--- src/test/java/uk/gov/hmcts/probate/health/ProbateHealthIndicatorTest.java
+++ src/test/java/uk/gov/hmcts/probate/health/ProbateHealthIndicatorTest.java
@@ -91,13 +91,13 @@ public class ProbateHealthIndicatorTest {
     public void shouldReturnStatusOfDownWhenUnknownHttpStatusCodeExceptionIsThrown() {
         final String statusText = "status text";
         when(mockRestTemplate.getForEntity(URL + endpoint, String.class))
-            .thenThrow(new UnknownHttpStatusCodeException(1000, statusText, null, null, null));
+            .thenThrow(new UnknownHttpStatusCodeException(999, statusText, null, null, null));
 
         Health health = businessHealthIndicator.health();
 
         assertThat(health.getStatus(), is(Status.DOWN));
         assertThat(health.getDetails().get("url"), is(URL));
-        assertThat(health.getDetails().get("message"), is("Unknown status code [1000] status text"));
+        assertThat(health.getDetails().get("message"), is("Unknown status code [999] status text"));
         assertThat(health.getDetails().get("exception"), is("UnknownHttpStatusCodeException - " + statusText));
     }
 
