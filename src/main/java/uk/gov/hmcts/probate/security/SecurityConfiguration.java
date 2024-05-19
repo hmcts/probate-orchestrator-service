d0ef03248a3c49591a9378c2fdbc38c51933a6b2
Michael1142 <michael.johnson9@justice.gov.uk>
2024-05-19 21:20:54 +0100

    Update security configuration for spring boot 3

--- src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/security/SecurityConfiguration.java
@@ -1,41 +1,66 @@
 package uk.gov.hmcts.probate.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import uk.gov.hmcts.reform.auth.checker.core.service.ServiceRequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
 import uk.gov.hmcts.reform.auth.checker.core.user.UserRequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.AuthCheckerServiceAndUserFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    private AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
-
-    private AuthenticationExceptionHandler authenticationExceptionHandler;
+    private final AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
+    private final AuthenticationExceptionHandler authenticationExceptionHandler;
 
     @Autowired
     public SecurityConfiguration(UserRequestAuthorizer<User> userRequestAuthorizer,
                                  ServiceRequestAuthorizer serviceRequestAuthorizer,
                                  AuthenticationManager authenticationManager,
                                  AuthenticationExceptionHandler authenticationExceptionHandler) {
-        authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer,
-            userRequestAuthorizer);
-        authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
+        this.authCheckerServiceAndUserFilter =
+                new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
+        this.authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
         this.authenticationExceptionHandler = authenticationExceptionHandler;
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html",
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(
+                        "/documents/**",
+                        "/generate/**",
+                        "/forms/**",
+                        "/invite/**",
+                        "/invites/**"
+                )
+                .addFilter(authCheckerServiceAndUserFilter)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .logout(logout -> logout.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(authenticationExceptionHandler)
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/swagger-ui.html",
                 "/swagger-ui/**",
                 "/swagger-resources/**",
                 "/v3/**",
@@ -44,29 +69,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 "/info",
                 "/migrateData",
                 "/favicon.ico",
-                "/");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .requestMatchers()
-                .antMatchers("/documents/**")
-                .antMatchers("/generate/**")
-                .antMatchers("/forms/**")
-                .antMatchers("/invite/**")
-                .antMatchers("/invites/**")
-                .and()
-                .addFilter(authCheckerServiceAndUserFilter)
-                .sessionManagement().sessionCreationPolicy(STATELESS).and()
-                .csrf().disable()
-                .formLogin().disable()
-                .logout().disable()
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationExceptionHandler);
+                "/"
+        );
     }
 }
