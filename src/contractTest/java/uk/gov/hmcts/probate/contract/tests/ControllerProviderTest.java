82dd337253393cbd93dbaf70d509c6afc6f43e9b
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-07-18 15:25:26 +0100

    set default url to https://pact-broker.platform.hmcts.net

--- src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/contract/tests/ControllerProviderTest.java
@@ -40,7 +40,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @PactBroker(
-    url = "https://pact-broker.platform.hmcts.net",
+    url = "${pact.broker.url:https://pact-broker.platform.hmcts.net}",
     consumerVersionSelectors = {
         @VersionSelector(tag = "${PACT_BRANCH_NAME:Dev}")
     },
