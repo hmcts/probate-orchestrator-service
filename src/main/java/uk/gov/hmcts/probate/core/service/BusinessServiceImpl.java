60e95e4a8f5d8a5b4810d386009df1ce5243052b
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-10-23 16:03:31 +0100

    DTSPB-4260 fix unused import

--- src/main/java/uk/gov/hmcts/probate/core/service/BusinessServiceImpl.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/BusinessServiceImpl.java
@@ -26,7 +26,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Slf4j
 @Component
