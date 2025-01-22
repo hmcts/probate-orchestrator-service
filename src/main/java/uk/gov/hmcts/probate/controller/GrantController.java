6ddb76484c6b71f582b4ea08856fef3328a05e4f
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2025-01-22 12:21:32 +0000

    remove unused import

--- src/main/java/uk/gov/hmcts/probate/controller/GrantController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/GrantController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
