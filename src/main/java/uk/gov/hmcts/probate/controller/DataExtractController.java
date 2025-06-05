4d23593a34bb8425b87deee38e9a006ec79d3a3b
Tom Saunders <tom.saunders@justice.gov.uk>
2025-06-05 09:10:06 +0100

    add handling to use feature flag to switch from orch to back-office.

--- src/main/java/uk/gov/hmcts/probate/controller/DataExtractController.java
+++ src/main/java/uk/gov/hmcts/probate/controller/DataExtractController.java
@@ -1,9 +1,8 @@
 package uk.gov.hmcts.probate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,12 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.probate.service.DataExtractService;
+import uk.gov.hmcts.probate.service.FeatureToggleService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
-@RequiredArgsConstructor
 @RequestMapping("/data-extract")
 @RestController
 @Tag(name = "Initiate data extract for HMRC, IronMountain and Excela")
@@ -25,6 +24,14 @@ public class DataExtractController {
 
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     private final DataExtractService dataExtractService;
+    private final FeatureToggleService featureToggleService;
+
+    public DataExtractController(
+            final DataExtractService dataExtractService,
+            final FeatureToggleService featureToggleService) {
+        this.dataExtractService = dataExtractService;
+        this.featureToggleService = featureToggleService;
+    }
 
     @Operation(summary = "Initiate HMRC data extract", description = "Will find cases for yesterdays date")
     @PostMapping(path = "/hmrc")
@@ -54,6 +61,10 @@ public class DataExtractController {
     @Operation(summary = "Initiate IronMountain data extract", description = "Will find cases for yesterdays date")
     @PostMapping(path = "/iron-mountain")
     public ResponseEntity initiateIronMountainExtract() {
+        if (featureToggleService.isIronMountainInBackOffice()) {
+            log.info("IronMountain extract task is feature toggled to run through back office");
+            return ResponseEntity.internalServerError().body("Iron Mountain extract task is run through back office");
+        }
 
         return initiateIronMountainExtract(DATE_FORMAT.format(LocalDate.now().minusDays(1L)));
     }
@@ -71,6 +82,10 @@ public class DataExtractController {
     @Operation(summary = "Initiate Excela data extract", description = "Will find cases for yesterdays date")
     @PostMapping(path = "/exela")
     public ResponseEntity initiateExelaExtract() {
+        if (featureToggleService.isExelaInBackOffice()) {
+            log.info("Exela extract task is feature toggled to run through back office");
+            return ResponseEntity.internalServerError().body("Exela extract task is run through back office");
+        }
         log.info("Extract initiated for Excela");
         return initiateExelaExtract(DATE_FORMAT.format(LocalDate.now().minusDays(1L)));
     }
@@ -87,9 +102,9 @@ public class DataExtractController {
     @Operation(summary = "Initiate Excela data extract", description = " Date MUST be in format 'yyyy-MM-dd'")
     @PostMapping(path = "/exela/{fromDate}/{toDate}")
     public ResponseEntity initiateExelaExtractDateRange(
-        @Parameter(name = "Date range to find cases against", required = true)
-        @PathVariable("fromDate") String fromDate,
-        @PathVariable("toDate") String toDate) {
+            @Parameter(name = "Date range to find cases against", required = true)
+            @PathVariable("fromDate") String fromDate,
+            @PathVariable("toDate") String toDate) {
 
         log.info("Calling perform Excela data extract from date...");
         return dataExtractService.initiateExelaExtractDateRange(fromDate, toDate);
@@ -98,9 +113,9 @@ public class DataExtractController {
     @Operation(summary = "Initiate Smee and Ford data extract", description = " Date MUST be in format 'yyyy-MM-dd'")
     @PostMapping(path = "/smee-and-ford/{fromDate}/{toDate}")
     public ResponseEntity initiateSmeeAndFordExtractDateRange(
-        @Parameter(name = "Date range to find cases against", required = true)
-        @PathVariable("fromDate") String fromDate,
-        @PathVariable("toDate") String toDate) {
+            @Parameter(name = "Date range to find cases against", required = true)
+            @PathVariable("fromDate") String fromDate,
+            @PathVariable("toDate") String toDate) {
 
         log.info("Calling perform Smee And Ford data extract from date, to date {} {}", fromDate, toDate);
         return dataExtractService.initiateSmeeAndFordExtractDateRange(fromDate, toDate);
--- src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerUnitTest.java
+++ src/test/java/uk/gov/hmcts/probate/controller/DataExtractControllerUnitTest.java
@@ -1,27 +1,53 @@
 package uk.gov.hmcts.probate.controller;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.probate.service.DataExtractService;
+import uk.gov.hmcts.probate.service.FeatureToggleService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 public class DataExtractControllerUnitTest {
 
     @Mock
     DataExtractService dataExtractService;
 
-    @InjectMocks
+    @Mock
+    FeatureToggleService featureToggleService;
+
+
     DataExtractController dataExtractController;
+
+    AutoCloseable closeableMocks;
+
+    @BeforeEach
+    void setUp() {
+        closeableMocks = MockitoAnnotations.openMocks(this);
+
+        when(featureToggleService.isIronMountainInBackOffice())
+                .thenReturn(false);
+        when(featureToggleService.isExelaInBackOffice())
+                .thenReturn(false);
+
+        dataExtractController = new DataExtractController(
+                dataExtractService,
+                featureToggleService);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeableMocks.close();
+    }
 
     @Test
     public void shouldInitiateSmeeAndFordDataExtractForDateRange() {
@@ -53,5 +79,29 @@ public class DataExtractControllerUnitTest {
         when(dataExtractService.initiateExelaExtractDateRange(anyString(), anyString())).thenReturn(responseEntity);
         ResponseEntity response = dataExtractController.initiateExelaExtractDateRange("2020-12-30", "2020-12-31");
         assertThat(response).isEqualTo(responseEntity);
+    }
+
+    @Test
+    void shouldNotInitiateIronMountainWhenInBackOffice() {
+        when(featureToggleService.isIronMountainInBackOffice())
+                .thenReturn(true);
+
+        ResponseEntity responseEntity = dataExtractController.initiateIronMountainExtract();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(500));
+        assertThat(responseEntity.getBody()).isEqualTo("Iron Mountain extract task is run through back office");
+        verifyNoInteractions(dataExtractService);
+    }
+
+    @Test
+    void shouldNotInitiateExelaWhenInBackOffice() {
+        when(featureToggleService.isExelaInBackOffice())
+                .thenReturn(true);
+
+        ResponseEntity responseEntity = dataExtractController.initiateExelaExtract();
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(500));
+        assertThat(responseEntity.getBody()).isEqualTo("Exela extract task is run through back office");
+        verifyNoInteractions(dataExtractService);
     }
 }
