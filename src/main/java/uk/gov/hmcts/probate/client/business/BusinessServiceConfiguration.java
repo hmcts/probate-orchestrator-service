46d240280c8717f06bb92c71c0d8f3ae651acf03
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-06-13 19:40:46 +0100

    DTSPB-4818 functional tests

--- src/main/java/uk/gov/hmcts/probate/client/business/BusinessServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/probate/client/business/BusinessServiceConfiguration.java
@@ -1,27 +1,25 @@
 package uk.gov.hmcts.probate.client.business;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import feign.Logger;
 import feign.codec.Encoder;
 import feign.jackson.JacksonEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-@Configuration
+
 public class BusinessServiceConfiguration {
 
     static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
-    private final ObjectMapper objectMapper;
-
-    public BusinessServiceConfiguration(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @Bean
     @Primary
     Encoder feignEncoder() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        objectMapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
         return new JacksonEncoder(objectMapper);
     }
 
