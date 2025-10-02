85cc174fee2c82bee718e506820b9c74491fd7b3
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-10-02 19:00:57 +0100

    DTSPB-4704 Intestacy child journey changes

---
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/ApplicantFamilyDetailsMapper.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.probate.core.service.mapper;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.FromApplicantFamilyDetails;
+import uk.gov.hmcts.probate.core.service.mapper.qualifiers.ToApplicantFamilyDetails;
+import uk.gov.hmcts.reform.probate.model.Relationship;
+import uk.gov.hmcts.reform.probate.model.cases.ApplicantFamilyDetails;
+import uk.gov.hmcts.reform.probate.model.forms.intestacy.FamilyDetails;
+
+@Component
+public class ApplicantFamilyDetailsMapper {
+    @ToApplicantFamilyDetails
+    public ApplicantFamilyDetails toApplicantDetails(FamilyDetails details) {
+        if (details == null) {
+            return null;
+        }
+        return ApplicantFamilyDetails.builder()
+                .relationshipToDeceased(Relationship.fromString(details.getRelationshipToDeceased()))
+                .childAdoptedIn(details.getChildAdoptedIn())
+                .childAdoptionInEnglandOrWales(details.getChildAdoptionInEnglandOrWales())
+                .childAdoptedOut(details.getChildAdoptedOut())
+                .grandchildAdoptedIn(details.getGrandchildAdoptedIn())
+                .grandchildAdoptionInEnglandOrWales(details.getGrandchildAdoptionInEnglandOrWales())
+                .grandchildAdoptedOut(details.getGrandchildAdoptedOut())
+                .build();
+    }
+
+    @FromApplicantFamilyDetails
+    public FamilyDetails fromApplicantDetails(ApplicantFamilyDetails details) {
+        if (details == null) {
+            return null;
+        }
+        return FamilyDetails.builder()
+                .relationshipToDeceased(details.getRelationshipToDeceased().getDescription())
+                .childAdoptedIn(details.getChildAdoptedIn())
+                .childAdoptionInEnglandOrWales(details.getChildAdoptionInEnglandOrWales())
+                .childAdoptedOut(details.getChildAdoptedOut())
+                .grandchildAdoptedIn(details.getGrandchildAdoptedIn())
+                .grandchildAdoptionInEnglandOrWales(details.getGrandchildAdoptionInEnglandOrWales())
+                .grandchildAdoptedOut(details.getGrandchildAdoptedOut())
+                .build();
+    }
+}
