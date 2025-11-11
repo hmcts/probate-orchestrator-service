73f67e4d4b94439d4dc6c18da88056d7ededb1e7
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2025-11-11 18:37:38 +0000

    DTSPB-4743: Add part 'a' fields and point to new commons

--- src/main/java/uk/gov/hmcts/probate/core/service/mapper/IntestacyMapper.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/mapper/IntestacyMapper.java
@@ -85,6 +85,10 @@ public interface IntestacyMapper extends FormMapper<GrantOfRepresentationData, I
     @Mapping(target = "childrenDied", source = "deceased.anyDeceasedChildrenDieBeforeDeceased")
     @Mapping(target = "anyDeceasedGrandChildrenUnderEighteen",
         source = "deceased.anyDeceasedGrandchildrenUnderEighteen")
+    @Mapping(target = "anyLivingParents",
+            source = "deceased.anyLivingParents")
+    @Mapping(target = "sameParentsAsDeceased",
+            source = "deceased.sameParentsAsDeceased")
     @Mapping(target = "deceasedSpouseNotApplyingReason", expression = "java(form.getApplicant()!= null && "
         + "form.getApplicant().getSpouseNotApplyingReason() != null ? "
         + "SpouseNotApplyingReason.fromString(form.getApplicant().getSpouseNotApplyingReason()) : null)")
