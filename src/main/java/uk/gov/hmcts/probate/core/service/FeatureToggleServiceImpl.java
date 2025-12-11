a089811343b77cc28a05ee434409c323b12190b5
Tom Saunders <tom.saunders@justice.gov.uk>
2025-12-11 12:59:24 +0000

    add logging to allow verification that feature toggles work as expected.

--- src/main/java/uk/gov/hmcts/probate/core/service/FeatureToggleServiceImpl.java
+++ src/main/java/uk/gov/hmcts/probate/core/service/FeatureToggleServiceImpl.java
@@ -6,12 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.probate.service.FeatureToggleService;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 @Slf4j
 public class FeatureToggleServiceImpl implements FeatureToggleService {
     private final LDClientInterface ldClient;
     private final LDContext ldContext;
 
+    private final AtomicBoolean cacheConfirmFeatureToggle;
+    private final AtomicInteger logLimit;
+
+    static final String CONFIRM_FEATURE = "probate-confirm-feature-toggle";
     static final String PAYMENT_LOOKUP_FEATURE = "probate-use-ccd-lookup-not-elastic-when-paying";
 
     public FeatureToggleServiceImpl(
@@ -19,6 +26,9 @@ public class FeatureToggleServiceImpl implements FeatureToggleService {
             final LDContext ldContext) {
         this.ldClient = ldClient;
         this.ldContext = ldContext;
+
+        this.cacheConfirmFeatureToggle = new AtomicBoolean(false);
+        this.logLimit = new AtomicInteger(0);
     }
 
     private boolean isFeatureToggleOn(
@@ -27,8 +37,21 @@ public class FeatureToggleServiceImpl implements FeatureToggleService {
         return this.ldClient.boolVariation(featureToggleCode, this.ldContext, defaultValue);
     }
 
+    void confirmFeatureToggle() {
+        final boolean confirmFeatureToggle = isFeatureToggleOn(CONFIRM_FEATURE, false);
+        final int currentLimit = logLimit.getAndIncrement();
+        if (cacheConfirmFeatureToggle.compareAndSet(!confirmFeatureToggle, confirmFeatureToggle)) {
+            log.info("confirmFeatureToggle is now {}, was previously inverted", confirmFeatureToggle);
+        } else {
+            if (currentLimit % 128 == 0) {
+                log.info("confirmFeatureToggle is {} (called {} times)", confirmFeatureToggle, currentLimit);
+            }
+        }
+    }
+
     @Override
     public boolean useCcdLookupForPayments() {
+        confirmFeatureToggle();
         return isFeatureToggleOn(PAYMENT_LOOKUP_FEATURE, false);
     }
 }
